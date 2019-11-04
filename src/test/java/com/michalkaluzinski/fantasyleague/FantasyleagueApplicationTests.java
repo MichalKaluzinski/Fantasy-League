d3b1d63914d44a1ae78cae48385c0d240bbe85c8
Michal Kaluzinski <mi.kaluzinski@gmail.com>
2019-11-04 15:41:58 +0100

    create project via springi initializr

---
+++ src/test/java/com/michalkaluzinski/fantasyleague/FantasyleagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michalkaluzinski.fantasyleague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyleagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
